7bc69ac437cfa6fb15b70ed2e400fc2815b0c1c1
moehami <m.aljaaly@yahoo.co.uk>
2024-11-05 09:40:33 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,9 @@ export default function Home() {
   return (
     <main className="flex min-h-screen items-center justify-center gap-2 dark:bg-gray-800">
       <h1 className="text-2xl dark:text-white">Flowbite React + Next.js</h1>
+          <Alert color="info">
+      <span className="font-medium">Info alert!</span> Change a few things up and try submitting again.
+    </Alert>
       <DarkThemeToggle />
     </main>
   );
